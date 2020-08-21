325bf0a65d97764241a4f75bc85ad17a1d49f90c
rakhiagr <rakhiagr2020@gmail.com>
2020-08-21 20:47:26 +0530

    Updates

---
+++ precache-manifest.f9c69da092b25045bc9d1addb23d5108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86945bb6c150576062bac71876302ffb",
+    "url": "/react-pokemon-game/index.html"
+  },
+  {
+    "revision": "3f5344dfe39d9a26afef",
+    "url": "/react-pokemon-game/static/css/main.0058ea95.chunk.css"
+  },
+  {
+    "revision": "f6c256ca51b9c056827b",
+    "url": "/react-pokemon-game/static/js/2.0b85763f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokemon-game/static/js/2.0b85763f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5344dfe39d9a26afef",
+    "url": "/react-pokemon-game/static/js/main.f082f5b6.chunk.js"
+  },
+  {
+    "revision": "e8f357b0d579a84568f8",
+    "url": "/react-pokemon-game/static/js/runtime-main.3c107dd2.js"
+  }
+]);
